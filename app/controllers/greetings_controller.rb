08b9a400783a9aad41dcc4047478a3acc083784f
Estebanmual <esteban.munozal@gmail.com>
2022-07-23 13:23:16 -0500

    Fixed linters errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,11 +1,11 @@
 class GreetingsController < ApplicationController
-    def index
-        greetings = Greeting.all
-        index = Random.rand(0..4)
-        @greeting = greetings[index]
-        respond_to do |format|
-        format.html { render 'index' }
-        format.json { render json: @greeting }
+  def index
+    greetings = Greeting.all
+    index = Random.rand(0..4)
+    @greeting = greetings[index]
+    respond_to do |format|
+      format.html { render 'index' }
+      format.json { render json: @greeting }
     end
-    end
+  end
 end
